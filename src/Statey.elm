2f22ac42d968d21c84719984b2753fabde3b2dee
Jack Franklin <jack@jackfranklin.net>
2016-01-10 21:36:15 +0000

    Write some elm docs

--- src/Statey.elm
+++ src/Statey.elm
@@ -1,27 +1,56 @@
-module Statey (..) where
+module Statey (StateyError(TransitionNotDefined, GuardPreventedTansition), StateMachine, State, StateRecord, makeState, getState, getStates, transition) where
 
-import Dict exposing (Dict)
-import List
+{-| This library provides a simple state machine implementation in Elm.
+
+# Making a state machine
+@docs StateMachine, makeState, State, StateRecord
+
+# Getting the state
+@docs getState, getStates
+
+# Transitioning
+@docs transition
+
+# Errors
+@docs StateyError
+-}
 
 
+{-| Represents an error in a state transition.
+-}
 type StateyError
     = TransitionNotDefined
     | GuardPreventedTansition
 
 
+{-| Represents a state. This can either be a user given state, or `AnyState`,
+which is used in guards to denote that it applies to any state
+-}
 type State
     = UserState String
     | AnyState
 
 
+{-| A `StateRecord` is just any record that has a `state : State` property on it. Every state machine is created with one of these as its type, and most of the functions expect to be given one.
+
+The records that you want to use in the state machine should be based off this:
+
+    type alias Person = StateRecord { name : String }
+-}
 type alias StateRecord a =
     { a | state : State }
 
 
+{-| You can guard transitions by defining guards. These are called when a transition occurs that matches the `from` and `to` properties, at which point `fn` will be called, along with the record that is being transitioned.
+
+    { from = startState, to = endState, fn: (\record -> record.name /= "Jack") }
+-}
 type alias Guard a =
     { from : State, to : State, fn : StateRecord a -> Bool }
 
 
+{-| A state machine contains a list of valid states, a list of transitions and a list of guards.
+-}
 type alias StateMachine a =
     { states : List State
     , transitions : List ( State, State )
@@ -29,16 +58,30 @@ type alias StateMachine a =
     }
 
 
+{-| Make a state from a string. This is the method you should use to create states
+for your state machine. The string given doesn't matter, just make it relevant to
+your application.
+
+    startState = makeState "start"
+-}
 makeState : String -> State
 makeState str =
     UserState str
 
 
+{-| given a particular record, return the state of it. This is an alias to `.state`
+currently, but you should use is in case the underlying abstractions change
+
+    getState { name = "jack", state = startState } == startState
+
+-}
 getState : StateRecord a -> State
 getState =
     .state
 
 
+{-| given a state machine, return its list of states. An alias to `.states`.
+-}
 getStates : StateMachine a -> List State
 getStates =
     .states
@@ -59,6 +102,28 @@ guardAllowsTransition stateMachine record newState =
         |> List.all (\{ fn } -> (fn record) == True)
 
 
+{-| Transition the given record from its state to the new state. The result will
+either be an `Ok newRecord` or `Err StateyErr`.
+
+    startState = makeState "start"
+    middleState = makeState "middle"
+    endState = makeState "end"
+
+    stateMachine : StateMachine { name : String }
+    stateMachine =
+        { states = [ startState, middleState, endState ]
+        , transitions = [ ( startState, middleState ), ( middleState, endState ) ]
+        , guards = []
+        }
+
+    person = { name = "Jack", state = startState }
+
+    case transition stateMachine person middleState of
+        Ok newPerson ->
+            newPerson
+        Err err ->
+            err == GuardPreventedTansition || err == TransitionNotDefined
+-}
 transition : StateMachine a -> StateRecord a -> State -> Result StateyError (StateRecord a)
 transition stateMachine record newState =
     if transitionDefined stateMachine ( record.state, newState ) then
